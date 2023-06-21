2039423b31d9123f64347f9a91f25f3d4a112dcd
kahlstorf1 <dkahlstorf1@gmail.com>
2023-06-21 09:46:28 -0400

    feat: initial Tailwind styling

--- app/studio/[studioID]/page.tsx
+++ app/studio/[studioID]/page.tsx
@@ -1,7 +1,11 @@
+import AddLocation from "../../../components/AddLocation";
+
 export default async function StudioPage() {
   return (
-    <div>
-      Specific Studio Goes Here
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <p className="text-3xl font-bold mb-4">Specific Studio Goes Here</p>
+
+      <AddLocation />
     </div>
   );
 }
---
+++ components/AddLocation.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function AddLocation() {
+  return (
+    //route needs to be dynamic, need from login
+    <Link href="/studio/1234/add-location">
+      <button type="button">Add a Location</button>
+    </Link>
+  );
+}
