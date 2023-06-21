6efabc059fc6f32d5bfed9775c6a6cd5650e515a
kahlstorf1 <dkahlstorf1@gmail.com>
2023-06-20 23:51:52 -0400

    bug: forgot to pull main, integrate Supabase

---
+++ lib/supabase.ts
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { createClient } from '@supabase/supabase-js';
+
+export default createClient(
+  process.env.NEXT_PUBLIC_DB_URL!,
+  process.env.NEXT_PUBLIC_DB_API_KEY!,
+);
