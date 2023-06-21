d240716f4535f93ee093ce777968a177f7606935
kahlstorf1 <dkahlstorf1@gmail.com>
2023-06-21 09:46:01 -0400

    temp: manually insert dynamic routes

--- app/studio/AddLocation.tsx
+++
@@ -1,9 +0,0 @@
-import Link from 'next/link';
-
-export default function AddLocation() {
-  return (
-    <Link href="/studio/add-location">
-      <button type="button">Add a Location</button>
-    </Link>
-  );
-}
