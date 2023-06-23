2bd692255f7f455bea7c794ec0cc4f6b90b236e9
Tim <timdobranski@gmail.com>
2023-06-22 20:58:45 -0700

    Skeleton added in app directory for all needed elements

--- app/studio/[studioID]/page.tsx
+++ app/studio/[studioID]/page.tsx
@@ -1,7 +1,18 @@
-export default async function StudioPage() {
+// eslint-disable-next-line import/extensions
+import Image from 'next/image';
+import Link from 'next/link';
+
+export default function StudioPage() {
   return (
-    <div>
-      Specific Studio Goes Here
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Studio Name</h1>
+      <Image src="/images/studioPlaceholder.png" alt="gym placeholder" width={400} height={300} />
+      <Link href="/"> Add A Class</Link>
+      <Link href="/"> Add A Location</Link>
+      <Link href="/"> View All Classes</Link>
+      <Link href="/"> View All Locations</Link>
+      <Link href="/"> View Metrics</Link>
+
+    </main>
   );
 }
