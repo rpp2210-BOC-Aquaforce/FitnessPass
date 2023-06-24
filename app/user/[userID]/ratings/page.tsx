89c10922628d7371c9dd00ecac4edc8b0a515166
Midori <midorili998@gmail.com>
2023-06-24 12:39:01 -0700

    CSS for nav bar

--- app/user/[userID]/ratings/page.tsx
+++ app/user/[userID]/ratings/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import Link from 'next/link';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { faCalendar, faHeart } from '@fortawesome/free-solid-svg-icons';
 import supabase from '../../../../lib/supabase';
 
 export default function Ratings() {
@@ -32,7 +36,6 @@ export default function Ratings() {
     num_ratings: 0,
     created_at: 'string',
   };
-  const [rating, setRating] = useState<number>(0);
   const [classes, setClasses] = useState<number>(0);
   const [classList, setClassList] = useState<ClassList>(defaultClass);
 
@@ -92,31 +95,25 @@ export default function Ratings() {
     getClasses();
   }, []);
 
-  // function displayRadioValue() {
-  //   const element = document.getElementsByName('rating');
-  //   for (let i = 0; i < element.length; i += 1) {
-  //     if (element[i].checked) {
-  //       // setValue(element[i].value);
-  //       updateRating(element[i].value);
-  //     }
-  //   }
-  // }
-
-  const onChange = (e:any) => {
-    console.log('changed', e.target.value);
-    setRating(e.target.value);
-    updateRating(rating);
-  };
+  function displayRadioValue() {
+    const elements = document.getElementsByName('rating') as NodeListOf<HTMLInputElement>;
+    for (let i = 0; i < elements.length; i += 1) {
+      if (elements[i].checked) {
+        const ratingValue = parseInt(elements[i].value, 10);
+        updateRating(ratingValue);
+      }
+    }
+  }
 
   return (
     <div className="text-2xl">
       <div className="flex justify-end md:w-1/3 mt-5">
         <Link href="/user/123/profile">My Profile</Link>
       </div>
-      <h1 className="relative m:inset-x-20 inset-y-4 text-5xl">My Ratings</h1>
+      <h1 className="relative mx-auto text-center text-5xl">My Ratings</h1>
       <div className="relative sm:inset-10 md:w-1/3">
         <div>
-          <label htmlFor="form" className="block mb-4">
+          <label htmlFor="form" className="block mb-4 mx-auto">
             Rate this class:
             <div className="bg-white p-4 mb-4">
               <p className="font-bold">Class Name:</p>
@@ -127,35 +124,53 @@ export default function Ratings() {
               <p>{classList.date}</p>
               <p className="font-bold">Instructor:</p>
               <p>{classList.instructor}</p>
+              <form className="inline-flex">
+                <div className="flex mb-4">
+                  {[1, 2, 3, 4, 5].map((value) => (
+                    <label htmlFor={`rating-${value}`} key={value} className="flex items-center mr-4">
+                      {value}
+                      <input
+                        name="rating"
+                        id={`rating-${value}`}
+                        type="radio"
+                        value={value}
+                        className="ml-1"
+                      />
+                    </label>
+                  ))}
+                </div>
+                <button type="button" className="ml-5 rounded-lg border border-black py-2 px-4 text-white hover:text-orangeLight border rounded-full border-white bg-orange" onClick={displayRadioValue}>
+                  Submit
+                </button>
+              </form>
             </div>
           </label>
-          <form>
-            <div className="flex mb-4">
-              {[1, 2, 3, 4, 5].map((value) => (
-                <label htmlFor={`rating-${value}`} key={value} className="flex items-center mr-4">
-                  {value}
-                  <input
-                    name="rating"
-                    id={`rating-${value}`}
-                    type="radio"
-                    value={value}
-                    onChange={onChange}
-                    className="ml-1"
-                  />
-                </label>
-              ))}
-            </div>
-            <button type="button" className="ml-5 rounded-lg border border-black py-2 px-4">
-              Submit
-            </button>
-          </form>
+
         </div>
       </div>
       <div className="relative sm:inset-10 flex sm:place-content-between md:w-1/3">
-        <Link href="/user/123/favorites">My Schedule</Link>
-        <Link href="/user/123/classes">Classes</Link>
-        <Link href="/user/123/favorites">My Favorites</Link>
+        <Link
+          href="/user/123/favorites"
+          className="text-white hover:text-orangeLight mr-2 md:mr-4 border rounded-full border-white py-1 px-2 md:py-1.5 md:px-3 bg-orange text-sm md:text-base"
+        >
+          My Schedule
+          <FontAwesomeIcon icon={faCalendar} className="mr-1 ml-1.5 md:mr-1 md:ml-2" />
+        </Link>
+        <Link
+          href="/user/123/classes"
+          className="text-white hover:text-orangeLight mr-2 md:mr-4 border rounded-full border-white py-1 px-2 md:py-1.5 md:px-3 bg-orange text-sm md:text-base"
+        >
+          Classes
+        </Link>
+        <Link
+          href="/user/123/favorites"
+          className="text-white hover:text-orangeLight border rounded-full border-white py-1 px-2 md:py-1.5 md:px-3 bg-orange text-sm md:text-base"
+        >
+          My Favorites
+          <FontAwesomeIcon icon={faHeart} className="mr-1 ml-1.5 md:mr-1 md:ml-2" />
+        </Link>
       </div>
+
     </div>
   );
 }
