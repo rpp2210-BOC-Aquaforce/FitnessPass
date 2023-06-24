52c4cfb09aec3c64d6778525aa0fe6406fa82fe7
George Liu <georgeliu8110@gmail.com>
2023-06-24 15:12:56 -0500

    newest push

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,6 +1,55 @@
-/* eslint-disable no-console */
-/* eslint-disable consistent-return */
-/* eslint-disable prefer-destructuring */
+// // JS version
+// import NextAuth from 'next-auth';
+// import CredentialsProvider from 'next-auth/providers/credentials';
+// import GoogleProvider from 'next-auth/providers/google';
+// import { checkPassword } from '../../../lib/auth';
+// import supabase from '../../../lib/supabase';
+
+// export default NextAuth({
+//   session: {
+//     strategy: 'jwt',
+//     // maxAge: 1 * 24 * 60 * 60, // 1d
+//   },
+//   providers: [
+//     CredentialsProvider({
+//       async authorize(credentials) {
+//         if (!credentials) {
+//           throw new Error('Missing credentials!');
+//         }
+//         const userEmail = credentials.email;
+//         const { data: users, error } = await supabase
+//           .from('users')
+//           .select('email, password')
+//           .eq('email', userEmail);
+
+//         if (error) {
+//           console.log(error);
+//         }
+
+//         if (users.length === 0) {
+//           throw new Error('No user found!');
+//         }
+
+//         const isValidPassword = await checkPassword(credentials.password, users[0].password);
+
+//         if (!isValidPassword) {
+//           throw new Error('your password is incorrect!');
+//         }
+//         // if return obnject inside authroize,
+//         // then we let NextAuth know that authroization succeed
+//         return { email: users[0].email };
+//         // info stored in session token
+//       },
+//     }),
+
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// });
+
+// TS version
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import GoogleProvider from 'next-auth/providers/google';
@@ -18,16 +67,21 @@ export default NextAuth({
         if (!credentials) {
           throw new Error('Missing credentials!');
         }
-        const email = credentials.email;
+        const userEmail = credentials.email;
         const { data: users, error } = await supabase
           .from('users')
           .select('email, password')
-          .eq('email', email);
-        console.log('user in auth', users);
-        if (error) {
-          return null;
+          .eq('email', userEmail);
+
+        if (users === null) {
+          throw new Error('No user found!');
         }
-        if (users.length === 0) {
+
+        if (error) {
+          console.log(error);
+        }
+
+        if (users && users.length === 0) {
           throw new Error('No user found!');
         }
 
@@ -36,7 +90,8 @@ export default NextAuth({
         if (!isValidPassword) {
           throw new Error('your password is incorrect!');
         }
-        // if return obnject inside authroize, then we let NextAuth know that authroization succeed
+        // if return obnject inside authroize,
+        // then we let NextAuth know that authroization succeed
         return { email: users[0].email };
         // info stored in session token
       },
