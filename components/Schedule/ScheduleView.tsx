b208b03bfc931a658d916d04c895b2f40d9defdd
Daniel Avila <messagedaniel@protonmail.com>
2023-06-24 14:14:04 -0400

    refactor(Schedule.tsx): import supabase and parseLocalDate from a new index file feat(date-fns.ts): add parseLocalDate function feat(index.ts): export supabase and parseLocalDate functions

--- components/Schedule/ScheduleView.tsx
+++ components/Schedule/ScheduleView.tsx
@@ -12,13 +12,9 @@ import {
 } from 'date-fns';
 import { FitnessClasses } from '../FitnessClasses';
 import { UserClass } from '../../types';
+import { parseLocalDate } from '../../lib';
 
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-const parseLocalDate = (dateString: string) => {
-  const [year, month, day] = dateString.split('-').map(Number);
-  // Note: JavaScript counts months from 0 (January is 0, February is 1, etc.)
-  return new Date(year, month - 1, day);
-};
 
 const getScheduledDates = (userClasses: UserClass[]) => {
   const scheduledDates = new Set();
