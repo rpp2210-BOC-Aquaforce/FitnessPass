83c633a046715f8fb2a4a7b03794feb32c00bda5
Tim <timdobranski@gmail.com>
2023-06-24 11:36:44 -0700

    Progress on populating studio profile with data

--- app/studio/[studioID]/view-locations/page.tsx
+++ app/studio/[studioID]/view-locations/page.tsx
@@ -1,13 +1,31 @@
+'use client';
+
 // import Location from './location';
+
+import React, { useEffect } from 'react';
 import styles from './page.module.css';
+import supabase from '../../../../lib/supabase';
 
 export default function StudioLocations() {
-  // const locationsArr = [];
+  const studioID = 1;
+  const fetchStudioLocations = async () => {
+    const { data, error } = await supabase
+      .from('locations')
+      .select('*')
+      .eq('studio_id', studioID);
+    if (error) {
+      console.error(error);
+    } else {
+      // Data
+      console.log('Fetch Data: ', data);
+      setStudioLocs(data);
+    }
+    console.log('Studio Locations: ', studioLocs);
+  };
 
-  // const allLocationComponents = locationsArr.map((location) => (
-  //   <Location loc={location}
-  //     key={location.id}/>));
-
+  useEffect(() => {
+    fetchStudioLocations();
+  }, []);
   return (
     <div className={styles.locationList}>
       <h1 className={styles.header}>All Locations</h1>
