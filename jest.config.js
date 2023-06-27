3a4cf56d1c7062bac041fb9734ff7439da5557b3
Daniel Avila <messagedaniel@protonmail.com>
2023-06-27 15:50:43 -0400

    test: add tests for ClassCard and ScheduleView components feat: replace useSession with getServerSession in schedule page fix: change authOptions import in schedule page chore: update jest configuration to include coverage reporting chore: add jest setup file to mock Swiper CSS and import testing-library dependencies chore: update dependencies in package.json
    refactor(ScheduleView.tsx): remove unused imports and functions
    feat(ScheduleView.tsx): add getWeekTitle, renderWeekSlides, getScheduledDates, and getNextScheduledClass functions from date-fns.ts
    test(date-fns.spec.tsx): add tests for getWeekTitle, renderWeekSlides, getScheduledDates, and getNextScheduledClass functions from date-fns.ts
    
    feat(date-fns.tsx): add utility functions for parsing and formatting dates, rendering week days and slides, and getting week title.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,39 @@
-module.exports = {
-  roots: ['<rootDir>'],
-  transform: {
-    '^.+\\.(js|jsx|ts|tsx)$': 'ts-jest',
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+// Add any custom config to be passed to Jest
+/** @type {import('@jest/types').Config.InitialOptions} */
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^@/src/(.*)$': '<rootDir>/$1',
+    'swiper/css': 'swiper/swiper.min.css',
   },
-  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testEnvironment: 'jest-environment-jsdom',
 };
+
+module.exports = async () => ({
+  /**
+   * Using ...(await createJestConfig(customJestConfig)()) to override transformIgnorePatterns
+   * provided byt next/jest.
+   *
+   * @link https://github.com/vercel/next.js/issues/36077#issuecomment-1096635363
+   */
+  ...(await createJestConfig(customJestConfig)()),
+  /**
+   * Swiper use ECMAScript Modules (ESM) and Jest provides some experimental support for it
+   * but "node_modules" are not transpiled by next/jest yet.
+   *
+   * The "transformIgnorePatterns" on "jest.config.js" prevents the Swiper files from being
+   * transformed by Jest but it affects the CSS files that are provided by this package.
+   * Mocking these CSS files is the solution that demands the smallest configuration.
+   *
+   * @link https://github.com/vercel/next.js/issues/36077#issuecomment-1096698456
+   * @link https://jestjs.io/docs/ecmascript-modules
+   */
+  transformIgnorePatterns: ['node_modules/(?!(swiper|ssr-window|dom7)/)'],
+});
--- src/components/Schedule/ScheduleView.tsx
+++ src/components/Schedule/ScheduleView.tsx
@@ -3,89 +3,22 @@
 import React, {
   useState, useEffect, useRef, useCallback,
 } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper } from 'swiper/react';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
 import {
-  format, addDays, startOfWeek, isSameDay, differenceInWeeks,
+  addDays, isSameDay, differenceInWeeks,
 } from 'date-fns';
 import { UserClass } from '@/lib/types';
-import { parseLocalDate } from '@/lib';
-import { FitnessClasses } from '../FitnessClasses';
-
-const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
-
-const getScheduledDates = (userClasses: UserClass[]) => {
-  const scheduledDates = new Set();
-  userClasses.forEach((userClass) => {
-    const classDate = parseLocalDate(userClass.classes.date);
-    scheduledDates.add(format(classDate, 'yyyy-MM-dd'));
-  });
-  return scheduledDates;
-};
-
-const renderWeekDays = (
-  startOfWeekDate: Date,
-  activeDay: Date,
-  setActiveDay: (date: Date) => void,
-  scheduledDates: Set<string | unknown>,
-) => days.map((day, index) => {
-  const date = addDays(startOfWeekDate, index);
-  const isActiveDay = isSameDay(activeDay, date);
-  const isScheduled = scheduledDates.has(format(date, 'yyyy-MM-dd'));
-
-  return (
-    <button
-      key={format(date, 'yyyy-MM-dd')}
-      type="button"
-      className={`flex flex-col items-center w-auto p-1 rounded-lg ${isActiveDay ? 'bg-seafoam text-white' : 'bg-gray-200'}`}
-      onClick={() => setActiveDay(date)}
-    >
-      <span className="flex text-sm font-semibold">{day}</span>
-      <span
-        className={`w-6 h-6 rounded-full flex items-center text-xs justify-center mt-2 ${
-          isActiveDay ? 'bg-white text-seafoam' : 'bg-seafoam text-white'
-        }`}
-      >
-        {date.getDate()}
-      </span>
-      {isScheduled && <span className="w-2 h-2 rounded-full bg-orange-500 mt-2" />}
-    </button>
-  );
-});
-
-type SetActiveDayFunction = React.Dispatch<React.SetStateAction<Date>>;
-
-const renderWeekSlides = (
-  numberOfWeeks: number,
-  activeDay: Date,
-  setActiveDay: SetActiveDayFunction,
-  scheduledDates: Set<string | unknown>,
-) => {
-  const weeks = [...Array(numberOfWeeks)].map((_, index) => {
-    const startOfWeekDate = startOfWeek(addDays(new Date(), (index - numberOfWeeks / 2) * 7));
-
-    return (
-      <SwiperSlide key={format(startOfWeekDate, 'yyyy-MM-dd')} className="flex w-full h-full">
-        <div className="flex-shrink flex space-x-3 justify-center ">{renderWeekDays(startOfWeekDate, activeDay, setActiveDay, scheduledDates)}</div>
-      </SwiperSlide>
-    );
-  });
-
-  return weeks;
-};
-
-const getNextScheduledClass = (userClasses: UserClass[]) => {
-  const currentDate = new Date();
-  for (let i = 0; i < userClasses.length; i += 1) {
-    const classDate = parseLocalDate(userClasses[i].classes.date);
-    if (classDate >= currentDate) {
-      return classDate;
-    }
-  }
-  return currentDate; // return current date if no future classes found
-};
+import {
+  parseLocalDate,
+  getScheduledDates,
+  getNextScheduledClass,
+  renderWeekSlides,
+  getWeekTitle,
+} from '@/lib/date-fns';
+import { FitnessClasses } from '@/components/FitnessClasses';
 
 type ScheduleViewProps = {
   userClasses: UserClass[];
@@ -114,16 +47,7 @@ export default function ScheduleView({ userClasses }: ScheduleViewProps) {
     gotoClassDate(nextScheduledClass);
   }, [userClasses, viewAll, gotoClassDate]);
 
-  const weekDifference = activeSlide - 26;
-  const plural = activeSlide === 27 || activeSlide === 25 ? '' : 's';
-  let weekTitle;
-  if (weekDifference < 0) {
-    weekTitle = `${Math.abs(weekDifference)} Week${plural} Ago`;
-  } else if (weekDifference > 0) {
-    weekTitle = `In ${weekDifference} Week${plural}`;
-  } else {
-    weekTitle = 'This Week';
-  }
+  const weekTitle = getWeekTitle(activeSlide);
 
   const classesForActiveDay = userClasses.filter((userClass) => {
     const classDate = parseLocalDate(userClass.classes.date);
