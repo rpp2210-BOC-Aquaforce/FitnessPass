6a828eea3f06cacbc43353484cb9a795cc6b73bc
Mallow <linlam0923@gmail.com>
2023-06-27 14:44:19 -0400

    created search bar inputs

--- app/user/[userID]/search/page.tsx
+++ app/user/[userID]/search/page.tsx
@@ -2,22 +2,26 @@
 
 import { useEffect, useState } from 'react';
 
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+// import Calendar from 'react-calendar';
+// import 'react-calendar/dist/Calendar.css';
 
-type ValuePiece = Date | null;
+// type ValuePiece = Date | null;
 
 export default function Search() {
   const [searchByClass, setSearchByClass] = useState('');
   const [searchByLocation, setSearchByLocation] = useState('');
-  const [selectedDate, setSelectedDate] = useState<ValuePiece|[ValuePiece, ValuePiece]>(new Date());
 
   const [showCalendar, setShowCalendar] = useState<boolean>(false);
 
-  const handleDateChange = (date: ValuePiece|[ValuePiece, ValuePiece]) => {
-    setSelectedDate(date);
-    setShowCalendar(false);
-  };
+  useEffect(() => {
+    console.log('searchByClass', searchByClass);
+    console.log('searchByLocation', searchByLocation);
+    console.log('showCalendar', showCalendar);
+  }, []);
+  // const handleDateChange = (date: ValuePiece|[ValuePiece, ValuePiece]) => {
+  //   setSelectedDate(date);
+  //   setShowCalendar(false);
+  // };
 
   return (
     <div className="text-black">
@@ -37,15 +41,15 @@ export default function Search() {
       <div className="flex">
         <input
           className="text-black text-center"
-          value={selectedDate instanceof Date
-            ? selectedDate.toISOString().split('T')[0]
-            : ''}
+          // value={selectedDate instanceof Date
+          //   ? selectedDate.toISOString().split('T')[0]
+          //   : ''}
           onFocus={() => setShowCalendar(true)}
           onChange={() => setShowCalendar(false)}
         />
         <button type="button" className="bg-blue-500 text-white">GO</button>
       </div>
-      {showCalendar ? (
+      {/* {showCalendar ? (
         <div>
           <Calendar
             className="fixed"
@@ -55,7 +59,7 @@ export default function Search() {
             onChange={handleDateChange}
           />
         </div>
-      ) : ''}
+      ) : ''} */}
     </div>
   );
 }
