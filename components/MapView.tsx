c55b78ae2fca6b2ed811992c6679bdbd0c41a0e8
Mallow <linlam0923@gmail.com>
2023-06-27 17:04:03 -0400

    build MapView

--- components/MapView.tsx
+++ components/MapView.tsx
@@ -1,16 +1,19 @@
+/* eslint-disable camelcase */
+
 'use client';
 
 import {
-  Dispatch, SetStateAction, useEffect,
+  Dispatch, SetStateAction, useEffect, useRef, useState,
 } from 'react';
-// import {
-//   Dispatch, SetStateAction, useEffect, useState, useRef,
-// } from 'react';
+
+import {
+  useLoadScript,
+} from '@react-google-maps/api';
 
 // import {
 //   useLoadScript, GoogleMap, Marker, InfoWindow,
 // } from '@react-google-maps/api';
-// import Geocode from 'react-geocode';
+import Geocode from 'react-geocode';
 
 type CLASS = {
   class_id: number,
@@ -29,11 +32,78 @@ interface MapProps {
   setList: Dispatch<SetStateAction<boolean>>;
 }
 
-export default function Map({ center, classes, setList }: MapProps) {
-  useEffect(() => {
-    console.log('center', center);
-  });
+type marker = {
+  class_id: number,
+  date: string,
+  duration: number,
+  instructor: string,
+  locations: {name: string, street: string, city: string, state: string, zip: string},
+  name: string,
+  time: string,
+  address: string,
+  position: LatLngLiteral,
+}
 
+export default function Map({ center, classes, setList }: MapProps) {
+  const [markers, setMarkers] = useState<marker[]>([]);
+
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: 'AIzaSyAxLZBfmUBWLcaK0WibtdtCWlmPvuB0Aws',
+  });
+  Geocode.setApiKey('AIzaSyAxLZBfmUBWLcaK0WibtdtCWlmPvuB0Aws');
+
+  const mapInstanceRef = useRef<google.maps.Map | null>(null);
+  if (isLoaded && mapInstanceRef.current && markers.length > 0) {
+    const bounds = new google.maps.LatLngBounds();
+    if (center) {
+      bounds.extend(center);
+    }
+    markers.forEach(({ position }) => {
+      bounds.extend(position);
+    });
+    mapInstanceRef.current.fitBounds(bounds);
+  }
+
+  useEffect(() => {
+    if (Array.isArray(classes)) {
+      classes.forEach(
+        ({
+          class_id,
+          name,
+          date,
+          time,
+          duration,
+          instructor,
+          locations,
+        }: CLASS) => {
+          const address = `${locations.street}, ${locations.city}, ${locations.state}${locations.zip}`;
+          Geocode.fromAddress(address).then(
+            (response) => {
+              const position = response.results[0].geometry.location;
+              const Class = {
+                class_id,
+                name,
+                date,
+                time,
+                duration,
+                instructor,
+                address,
+                position,
+                locations,
+              };
+              setMarkers((prevMarkers) => [...prevMarkers, Class]);
+            },
+            (error) => {
+              console.error(error);
+            },
+          );
+        },
+      );
+    }
+  }, []);
+
+  if (!Array.isArray(classes) || classes.length === 0) return <div>0 result</div>;
+  if (!isLoaded) return <div>loading...</div>;
   return (
     <div>
       <div>
