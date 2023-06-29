c636c837898467a97cdd1722e2f8b5f5a4eb938c
Rachel Quirbach <rachelquirbachy@gmail.com>
2023-06-28 23:37:37 -0400

    "Commit linter modifications for getLocations in lib/api.ts"

--- lib/api.ts
+++ lib/api.ts
@@ -1,24 +1,23 @@
-export const fetcher = async ({url, method, body, json = true}) => {
+// eslint-disable-next-line consistent-return
+export const fetcher = async ({ url, method, body, json = true }) => {
   const res = await fetch(url, {
     method,
     ...(body && {body: JSON.stringify(body)}),
     headers: {
       Accept: 'application/json',
-      'Content-Type': 'application/json'
-    }
-  })
+      'Content-Type': 'application/json',
+    },
+  });
 
   if (!res.ok) {
-    // handle your errors
-    throw new Error('API error')
+    throw new Error('API error');
   }
 
   if (json) {
-    const data = await res.json()
-    return data.data
+    const data = await res.json();
+    return data.data;
   }
-}
+};
 
-export const getLocations = (studio) => {
-  return fetcher({url: '/api/fetch-studio-locs', method: 'post', body: { studioId: studio }}) // Would like to refactor to a get method
-}
+// Would like to refactor to a get method
+export const getLocations = (studio: string) => fetcher({ url: '/api/fetch-studio-locs', method: 'post', body: { studioId: studio } });
