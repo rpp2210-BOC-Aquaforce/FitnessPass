11aa0128c097411c9a8ef5ce91b9089b7fbeb3fb
Rachel Quirbach <113475539+rquirbach@users.noreply.github.com>
2023-07-01 09:42:45 -0400

    "Commit rendering of Class Popularity chart with test data"

--- src/components/Metrics/MetricsView.tsx
+++ src/components/Metrics/MetricsView.tsx
@@ -1,13 +1,36 @@
 'use client';
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PopularityMetrics from './PopularityMetrics';
 
 export default function MetricsView() {
+  const [view, setView] = useState('popularity');
+
+  const handleClick = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    view === 'popularity' ? setView('engagement') : setView('popularity');
+  };
+
+  useEffect(() => {
+    setView(view);
+  }, [view]);
   return (
     <div>
       <h4>Hi I am the metrics view component</h4>
-      {/* <PopularityMetrics /> */}
+      {view === 'popularity' ? (
+        <div>
+          <h5>Class Popularity</h5>
+          <PopularityMetrics />
+          <button type="button" onClick={handleClick}>View Class Engagement</button>
+        </div>
+      ) : null}
+      {view === 'engagement' ? (
+        <div>
+          <h5>Class Engagement</h5>
+          {/* <EngagementMetrics /> */}
+          <button type="button" onClick={handleClick}>View Class Popularity</button>
+        </div>
+      ) : null}
     </div>
   );
 };
--- src/components/Metrics/PopularityMetrics.tsx
+++ src/components/Metrics/PopularityMetrics.tsx
@@ -1,38 +1,31 @@
-// 'use client';
+import React from 'react';
 
-// import React from 'react';
-// import { Doughnut } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'react-chartjs-2';
 
-// const data = {
-//   labels: [
-//     'Red',
-//     'Green',
-//     'Yellow'
-//   ],
-//   datasets: [{
-//     data: [300, 50, 100],
-//     backgroundColor: [
-//       '#FF6384',
-//       '#36A2EB',
-//       '#FFCE56',
-//     ],
-//     hoverBackgroundColor: [
-//       '#FF6384',
-//       '#36A2EB',
-//       '#FFCE56',
-//     ],
-//   }],
-// };
+ChartJS.register(ArcElement, Tooltip, Legend);
 
-// export default function PopularityMetrics() {
-//   return (
-//     <div>
-//       <h2>Doughnut Example</h2>
-//       <Doughnut
-//         data={data}
-//         width={400}
-//         height={400}
-//       />
-//     </div>
-//   );
-// }
+export const data = {
+  labels: ["Zumba", "HIIT Core", "Yoga Flow"],
+  datasets: [
+    {
+      label: "# of Attendees",
+      data: [30, 6, 12],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(255, 206, 86, 0.2)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+      ],
+      borderWidth: 1
+    }
+  ]
+};
+
+export default function PopularityMetrics() {
+  return <Doughnut data={data} />;
+}
