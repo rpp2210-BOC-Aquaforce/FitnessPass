6de03dfd2eed2ae04d20f9904467d3ac0a055342
Rachel Quirbach <113475539+rquirbach@users.noreply.github.com>
2023-07-03 15:26:23 -0400

    "Commit re-commenting in Metrics page link from Studio Profile Page"

--- app/studio/[studioID]/page.tsx
+++ app/studio/[studioID]/page.tsx
@@ -60,8 +60,8 @@ export default async function StudioPage() {
         <br />
         <Link href={`/studio/${studioID}/addclass`} className={styles.links}>Add A Class</Link>
         <br />
-        {/* <Link href={`/studio/${studioID}/metrics`} className={styles.links}>View Metrics</Link>
-        <br /> */}
+        <Link href={`/studio/${studioID}/metrics`} className={styles.links}>View Metrics</Link>
+        <br />
         <Link href={`/studio/${studioID}/view-classes`} className={styles.links}>View All Classes</Link>
         <br />
         <Link href={`/studio/${studioID}/view-locations`} className={styles.links}> View All Locations</Link>
