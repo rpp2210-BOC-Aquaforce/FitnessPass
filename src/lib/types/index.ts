d1c9a360c939f3563ecfce5678512976b91fc6a3
Rachel Quirbach <113475539+rquirbach@users.noreply.github.com>
2023-07-03 15:20:56 -0400

    "Adjusted lib/types/index.ts for linter errors"

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,94 +1,9 @@
 import { Session } from 'next-auth';
 
-export type ReactChildren = {
-  children: React.ReactNode;
-}
-
-export interface Location {
-  name: string | null;
-  photo_url?: string | null;
-}
-
-export interface Class {
-  class_id: number;
-  location_id: number | null;
-  name: string | null;
-  description: string | null;
-  date: string;
-  time: string | null;
-  duration: number | null;
-  tags: string | null; // Assuming tags is an array of strings
-  instructor: string | null;
-  total_rating: number | null;
-  num_ratings: number | null;
-  created_at: Date | null;
-  locations: Location;
-}
-
-export interface UserClass {
-  id: number;
-  user_id: number;
-  class_id: number;
-  class_rating: number | null;
-  favorite: boolean;
-  created_at: Date | null;
-  classes: Class;
-}
-
-export interface UserClasses {
-  userClasses: UserClass[] | [];
-}
-
-export interface CustomSession extends Session {
-  user: {
-    id: string;
-    name?: string;
-    email?: string;
-    image?: string;
-  } & Session['user'];
-}
-
-export interface StudioAddClass {
-  [key: string]: unknown;
-  loc_id: string;
-  location: string;
-  class_name: string;
-  class_description: string;
-  class_date: string;
-  class_start: string;
-  class_duration: number;
-  class_tags: string;
-  instructor: string;
-}
-
-export interface studioPopMetric {
-  name: string | null;
-  popularity: number | null;
-}
-
-export interface studioEngMetric {
-  week: string | null;
-  attendance: number | null;
-}
-import { Session } from 'next-auth';
-
 export * from './ratings';
 
 export type ReactChildren = {
   children: React.ReactNode;
-}
-
-export type UpdateRatingArgs = {
-  userId: string,
-  rating: number,
-  classId: number,
-}
-
-export type UpdateUserClassArgs = {
-  userId: string,
-  classId: number,
-  key: string,
-  value: boolean | number | string | null,
 }
 
 export interface Location {
@@ -148,4 +63,27 @@ export interface StudioAddClass {
   class_duration: number;
   class_tags: string;
   instructor: string;
+}
+
+export interface studioPopMetric {
+  name: string | null;
+  popularity: number | null;
+}
+
+export interface studioEngMetric {
+  week: string | null;
+  attendance: number | null;
+}
+
+export type UpdateRatingArgs = {
+  userId: string,
+  rating: number,
+  classId: number,
+}
+
+export type UpdateUserClassArgs = {
+  userId: string,
+  classId: number,
+  key: string,
+  value: boolean | number | string | null,
 }
