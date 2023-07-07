2219a17bbe5e6adc52bf3ff6fa6b3e25708251f6
kahlstorf1 <dkahlstorf1@gmail.com>
2023-07-06 22:39:51 -0400

    feat: Style Add Location form identical to other Studio Form

--- app/studio/[studioID]/add-location/page.tsx
+++ app/studio/[studioID]/add-location/page.tsx
@@ -57,6 +57,7 @@ function StudioLocationForm() {
       if (error) {
         console.error('Supabase Error: ', error);
       } else {
+        console.log(newStudio);
         router.push(`/studio/${studioID}`);
       }
     } catch (err) {
@@ -65,77 +66,96 @@ function StudioLocationForm() {
   };
 
   return (
-    <form className="max-w-md mx-auto" onSubmit={handleSubmit}>
-      <label htmlFor="studioName" className="block mb-2">
-        Studio Name:
-        <input
-          id="studioName"
-          type="text"
-          name="studioName"
-          value={formData.studioName}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="street" className="block mb-2">
-        Street:
-        <input
-          id="street"
-          type="text"
-          name="street"
-          value={formData.street}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="city" className="block mb-2">
-        City:
-        <input
-          id="city"
-          type="text"
-          name="city"
-          value={formData.city}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="state" className="block mb-2">
-        State:
-        <input
-          id="state"
-          type="text"
-          name="state"
-          value={formData.state}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="zip" className="block mb-2">
-        Zip Code:
-        <input
-          id="zip"
-          type="text"
-          name="zip"
-          value={formData.zip}
-          onChange={handleInputChange}
-        />
-      </label>
-      <label htmlFor="phone" className="block mb-2">
-        Phone Number:
-        <input
-          id="phone"
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleInputChange}
-        />
-      </label>
+    <div className="flex flex-col content-start justify-center items-center py-4 gap-y-4">
+      <h1 className="mt-4 text-3xl text-orange-500 font-semibold tracking-wider" data-testid="location_form_title">Add Location</h1>
+      <form className="flex flex-col gap-y-2" onSubmit={handleSubmit}>
+        <label htmlFor="studioName" className="block mb-2">
+          Studio Name:
+          <input
+            id="studioName"
+            type="text"
+            name="studioName"
+            value={formData.studioName}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <label htmlFor="street" className="block mb-2">
+          Street:
+          <input
+            id="street"
+            type="text"
+            name="street"
+            value={formData.street}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <label htmlFor="city" className="block mb-2">
+          City:
+          <input
+            id="city"
+            type="text"
+            name="city"
+            value={formData.city}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <label htmlFor="state" className="block mb-2">
+          State:
+          <input
+            id="state"
+            type="text"
+            name="state"
+            value={formData.state}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <label htmlFor="zip" className="block mb-2">
+          Zip Code:
+          <input
+            id="zip"
+            type="text"
+            name="zip"
+            value={formData.zip}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <label htmlFor="phone" className="block mb-2">
+          Phone Number:
+          <input
+            id="phone"
+            type="text"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            className="shadow appearance-none border rounded-sm border-teal-300 w-full py-2 px-3 text-gray-800 leading-tight focus:outline-none focus:shadow-outline"
+            required
+          />
+        </label>
+        <button
+          type="submit"
+          className="mt-6 bg-orange-500 hover:bg-orange-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Add Location
+        </button>
+      </form>
       <button
-        type="submit"
-        className="bg-orange-500 text-orange px-4 py-2 rounded-md mt-4"
+        type="button"
+        onClick={() => router.push(`/studio/${studioID}`)}
+        className="inline-block align-baseline font-bold text-sm text-orange-500 hover:text-orange-600"
       >
-        {' '}
-        Submit
-        {' '}
-
+        Cancel
       </button>
-    </form>
+    </div>
   );
 }
 
