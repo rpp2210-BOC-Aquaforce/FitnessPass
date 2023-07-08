d4e354b034d0b1d32ef22fb1df85625a5d9655f1
George Liu <georgeliu8110@gmail.com>
2023-07-07 21:50:33 -0500

    change homepage for signed in user

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import { SearchBar } from '@/components/index';
+import { SearchBar, Authform } from '@/components/index';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/lib/auth';
+import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  return (
+  const session = await getServerSession(authOptions);
 
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <SearchBar />
-      <h1>
-        Welcome To FitnessPass!
-      </h1>
-    </main>
-  );
+  const userId = (session?.user as any)?.id;
+  const isStudio = (session?.user as any)?.studio_user;
+
+  if (isStudio) {
+    redirect(`/studio/${userId}`);
+  } else if (!isStudio) {
+    redirect(`/user/${userId}/profile`);
+  } else {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <SearchBar />
+        <Authform />
+        <h1>
+          Welcome To FitnessPass!
+        </h1>
+      </main>
+    );
+  }
 }
